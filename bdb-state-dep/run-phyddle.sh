e117a67d68d071dfd3fe2e8afca4a8c91d23c012
Jamie R. Oaks <joaks1@gmail.com>
2025-03-07 17:04:18 -0600

    Switching to micromamba in shell scripts.

--- bdb-state-dep/run-phyddle.sh
+++ bdb-state-dep/run-phyddle.sh
@@ -4,11 +4,4 @@ set -e
 
 num_proc=30
 
-
-if [ -n "$(command -v conda)" ]
-then
-    eval "$(conda shell.bash hook)"
-    conda activate SDSD-phyddle
-fi
-
-phyddle -c phyddle_config.py --num_proc "$num_proc"
+micromamba run -n SDSD-phyddle phyddle -c phyddle_config.py --num_proc "$num_proc"
--- setup_project_env.sh
+++ setup_project_env.sh
@@ -16,35 +16,22 @@ cd "$project_dir"
 conda_env_path="${project_dir}/env/conda-env.yml"
 conda_env_name="SDSD-phyddle"
 
-conda_exe="conda"
-if [ -n "$(command -v mamba)" ]
-then
-    conda_exe="mamba"
-fi
-
 echo ""
 echo "Creating project conda environment using '$(which $conda_exe)'..."
 echo ""
-"$conda_exe" env create --name "$conda_env_name" --file "$conda_env_path"
-
-
-echo ""
-echo "Activating project conda environment..."
-echo ""
-eval "$(conda shell.bash hook)"
-conda activate "$conda_env_name"
+micromamba env create --name "$conda_env_name" --file "$conda_env_path"
 
 echo ""
 echo "Adding pydot_ng requirement via pip..."
 echo ""
-python -m pip install pydot_ng
+micromamba run -n SDSD-phyddle python -m pip install pydot_ng
 
 if [ -e "SDSDsim" ]
 then
     (
         cd SDSDsim
         echo "Installing existing SDSDsim within cond env '$conda_env_name' using '$(which python)'"
-        python -m pip install -e .
+        micromamba run -n SDSD-phyddle python -m pip install -e .
     )
 else
     echo ""
@@ -56,7 +43,7 @@ else
         git checkout main
         git pull
         echo "Installing SDSDsim within cond env '$conda_env_name' using '$(which python)'"
-        python -m pip install -e .
+        micromamba run -n SDSD-phyddle python -m pip install -e .
     )
 fi
 
@@ -65,7 +52,7 @@ then
     (
         cd phyddle
         echo "Installing existing phyddle within cond env '$conda_env_name' using '$(which python)'"
-        python -m pip install -e .
+        micromamba run -n SDSD-phyddle python -m pip install -e .
     )
 else
     echo ""
@@ -77,6 +64,6 @@ else
         git checkout main
         git pull
         echo "Installing phyddle within cond env '$conda_env_name' using '$(which python)'"
-        python -m pip install -e .
+        micromamba run -n SDSD-phyddle python -m pip install -e .
     )
 fi
