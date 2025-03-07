bc763f1eb97d9c1ac15f62f07d5277f28a00f7bd
Jamie R. Oaks <joaks1@gmail.com>
2025-03-07 16:18:44 -0600

    Adding phyddle configs and simulator scripts.

---
+++ bdb-simple/phyddle_config.py
@@ -0,0 +1,82 @@
+#====================================================================#
+# Default phyddle config file                                        #
+#====================================================================#
+
+args = {
+
+    #-------------------------------#
+    # Project organization          #
+    #-------------------------------#
+    'step'    : 'SFTEP',                   # Step(s) to run
+    'verbose' : 'T',                       # print verbose phyddle output?
+    'prefix'  : 'out',                     # Prefix for output for all setps
+    'dir'     : './',
+    'output_precision'   : 12,             # Number of digits (precision) for numbers in output files
+
+    #-------------------------------#
+    # Multiprocessing               #
+    #-------------------------------#
+    'use_parallel'   : 'T',                 # use multiprocessing to speed up jobs?
+    'use_cuda'       : 'T',
+    'num_proc'       : 30,                  # how many CPUs to use (-2 means all but 2)
+
+    #-------------------------------#
+    # Simulate Step settings        #
+    #-------------------------------#
+    'sim_command'       : f'python sim_bdb.py', # exact command string, argument is output file prefix
+    'sim_logging'       : 'verbose',        # verbose, compressed, or clean
+    'start_idx'         : 0,                # first simulation replicate index
+    'end_idx'           : 100000,           # last simulation replicate index
+    'sim_batch_size'    : 100,
+
+    #-------------------------------#
+    # Format Step settings          #
+    #-------------------------------#
+    'encode_all_sim'    : 'T',
+    'num_char'          : 1,                # number of evolutionary characters
+    'num_states'        : 2,                # number of states per character
+    'min_num_taxa'      : 10,               # min number of taxa for valid sim
+    'max_num_taxa'      : 1000,             # max number of taxa for valid sim
+    'tree_width'        : 500,              # tree width category used to train network
+    'tree_encode'       : 'extant',         # use model with serial or extant tree
+    'brlen_encode'      : 'height_brlen',   # how to encode phylo brlen? height_only or height_brlen
+    'char_encode'       : 'integer',        # how to encode discrete states? one_hot or integer
+    'param_est'         : {                 # model parameters to predict (labels)
+      'log10_birth_rate_0'           : 'num',
+      'log10_death_rate_0'           : 'num',
+      'log10_expected_burst_rate_0'  : 'num',
+    },                                      # Model parameters and variables to estimate
+    'param_data'         : {},              # Model parameters and variables treated as data
+    'tensor_format'     : 'hdf5',           # save as compressed HDF5 or raw csv
+    'char_format'       : 'csv',
+    'save_phyenc_csv'   : 'F',              # save intermediate phylo-state vectors to file
+
+    #-------------------------------#
+    # Train Step settings           #
+    #-------------------------------#
+    'num_epochs'        : 200,              # number of training intervals (epochs)
+    'num_early_stop'    : 3,
+    'prop_test'         : 0.05,             # proportion of sims in test dataset
+    'prop_val'          : 0.05,             # proportion of sims in validation dataset
+    'prop_cal'          : 0.20,             # proportion of sims in CPI calibration dataset
+    'cpi_coverage'      : 0.95,             # coverage level for CPIs
+    'cpi_asymmetric'    : 'T',              # upper/lower ('T') or symmetric ('F') CPI adjustments
+    'trn_batch_size'    : 1024,             # number of samples in each training batch
+    'loss_numerical'    : 'mse',            # loss function for learning (real-valued) targets
+    'optimizer'         : 'adam',           # optimizer for network weight/bias parameters
+
+    #-------------------------------#
+    # Estimate Step settings        #
+    #-------------------------------#
+
+    #-------------------------------#
+    # Plot Step settings            #
+    #-------------------------------#
+    'plot_train_color'      : 'blue',       # plot color for training data
+    'plot_test_color'       : 'purple',     # plot color for test data
+    'plot_val_color'        : 'red',        # plot color for validation data
+    'plot_aux_color'        : 'green',      # plot color for input auxiliary data
+    'plot_label_color'      : 'orange',     # plot color for labels (params)
+    'plot_emp_color'        : 'black',      # plot color for estimated data/values
+    'plot_pca_noise'        : 0.01          # Add noise to PCA plot to dampen contrast from point values
+ }
---
+++ bdb-state-dep/sim_bdb.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import math
+import numpy as np
+import scipy.stats as st
+
+import sdsdsim.model
+
+
+class FixedRV(object):
+    def __init__(self, x):
+        self.x = x
+
+    def rvs(self, *args, **kwargs):
+        return self.x
+
+
+class Priors(object):
+    def __init__(
+        self,
+        state_rate_prop = st.uniform(0.1, 0.6),
+        log_birth_rate = st.uniform(-2.0, 2.0),
+        log_death_rate_prop = st.uniform(-2.0, 2.0),
+        burst_rate_prop = st.uniform(0.2, 0.8),
+        burst_prob_0 = st.uniform(0.65, 0.35),
+        burst_prob_1 = st.uniform(0.0, 0.25),
+        model = st.randint(0, 2),
+        root_state = st.randint(0, 2),
+    ):
+        self.state_rate_prop = state_rate_prop
+        self.log_birth_rate = log_birth_rate
+        self.log_death_rate_prop = log_death_rate_prop
+        self.burst_rate_prop = burst_rate_prop
+        self.burst_prob_0 = burst_prob_0
+        self.burst_prob_1 = burst_prob_1
+        self.model = model 
+        self.root_state = root_state
+
+
+def draw_bdb_model(priors, rng):
+    log_birth_rate_0 = priors.log_birth_rate.rvs(size = None, random_state = rng)
+    birth_rate_0 = 10**log_birth_rate_0
+
+    log_birth_rate_1 = log_birth_rate_0
+    birth_rate_1 = birth_rate_0
+
+    log_death_rate_mult = priors.log_death_rate_prop.rvs(size = None, random_state = rng)
+    death_rate_mult = 10**log_death_rate_mult
+    death_rate = min([birth_rate_0, birth_rate_1]) * death_rate_mult
+
+    burst_rate_mult = priors.burst_rate_prop.rvs(size = None, random_state = rng)
+    burst_rate = min([birth_rate_0, birth_rate_1]) * burst_rate_mult
+
+    state_rate_mult = priors.state_rate_prop.rvs(size = None, random_state = rng)
+    state_rate = min([birth_rate_0, birth_rate_1]) * state_rate_mult
+    
+    burst_prob_0 = priors.burst_prob_0.rvs(size = None, random_state = rng)
+    burst_prob_1 = burst_prob_0
+    model_type = priors.model.rvs(size = None, random_state = rng)
+    if model_type == 1:
+        burst_prob_1 = priors.burst_prob_1.rvs(size = None, random_state = rng)
+
+    root_state = priors.root_state.rvs(size = None, random_state = rng)
+
+    model = sdsdsim.model.SDSDModel(
+        q = [
+            [ -state_rate,  state_rate ],
+            [  state_rate, -state_rate ],
+        ],
+        birth_rates = [birth_rate_0, birth_rate_1],
+        death_rates = [death_rate, death_rate],
+        burst_rate = burst_rate,
+        burst_probs = [burst_prob_0, burst_prob_1],
+        burst_furcation_poisson_means = [1.0, 1.0],
+        burst_furcation_poisson_shifts = [2, 2],
+        only_bifurcate = True,
+    )
+    return model, model_type, root_state
+
+def parse_cli_args():
+    out_path     = sys.argv[1]
+    prefix       = sys.argv[2]
+    sim_idx      = int(sys.argv[3])
+    batch_size   = int(sys.argv[4])
+    return out_path, prefix, sim_idx, batch_size
+
+def stream_tip_states(out_stream, root, all_zeros = False):
+    out_stream.write("taxa,data\n")
+    for leaf in root.leaf_iter():
+        if all_zeros:
+            out_stream.write(f"{leaf.label},0\n")
+        else:
+            out_stream.write(f"{leaf.label},{leaf.leafward_state}\n")
+
+def write_tip_states(path, root, all_zeros = False):
+    with open(path, 'w') as out_stream:
+        stream_tip_states(out_stream, root, all_zeros)
+
+def stream_variables(out_stream, var_dict):
+    names = list(sorted(var_dict.keys()))
+    header = ",".join(names)
+    vals = ",".join(str(var_dict[n]) for n in names)
+    out_stream.write(f"{header}\n{vals}\n")
+
+def write_variables(path, var_dict):
+    with open(path, 'w') as out_stream:
+        stream_variables(out_stream, var_dict)
+
+def get_model_parameters(sdsd_model):
+    return {
+        'state_rate_01'         : sdsd_model.ctmc.q[0][1],
+        'state_rate_10'         : sdsd_model.ctmc.q[1][0],
+        'birth_rate_0'          : sdsd_model.birth_rates[0],
+        'birth_rate_1'          : sdsd_model.birth_rates[1],
+        'death_rate_0'          : sdsd_model.death_rates[0],
+        'death_rate_1'          : sdsd_model.death_rates[1],
+        'burst_rate'            : sdsd_model.burst_rate,
+        'burst_prob_0'          : sdsd_model.burst_probs[0],
+        'burst_prob_1'          : sdsd_model.burst_probs[1],
+        'expected_burst_rate_0' : sdsd_model.burst_rate * sdsd_model.burst_probs[0],
+        'expected_burst_rate_1' : sdsd_model.burst_rate * sdsd_model.burst_probs[1],
+    }
+
+def get_log10_model_parameters(sdsd_model):
+    p = {}
+    for k, v in get_model_parameters(sdsd_model).items():
+        p[f'log10_{k}'] = math.log10(v)
+    return p
+
+def main():
+    out_path, prefix, start_idx, batch_size = parse_cli_args()
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
+
+    path_prefix = os.path.join(out_path, prefix)
+
+    rng = np.random.default_rng(start_idx)
+
+    root_state = 0
+    priors = Priors()
+
+    include_root_annotations = False
+    keep_extinct_trees = False
+    max_extant_leaves = 500
+    tree_width = 500
+    max_total_leaves = None
+    max_extinct_leaves = None
+    max_time = None
+    max_leaves_strict = True
+    # Phyddle does this according to the tree_encode config setting
+    prune_extinct_leaves = False
+
+    # Output everything and control use of variables with Phyddle config
+    label_names = [
+        'log10_state_rate_01',
+        'log10_state_rate_10',
+        'log10_birth_rate_0',
+        'log10_birth_rate_1',
+        'log10_death_rate_0',
+        'log10_death_rate_1',
+        'log10_burst_rate',
+        'log10_burst_prob_0',
+        'log10_burst_prob_1',
+        'log10_expected_burst_rate_0',
+        'log10_expected_burst_rate_1',
+    ]
+
+    sim_idx = start_idx
+
+    while sim_idx < (start_idx + batch_size):
+        sim_path_prefix = f'{path_prefix}.{sim_idx}'
+        tree_path = f'{sim_path_prefix}.tre'
+        data_path = f'{sim_path_prefix}.dat.csv'
+        labels_path = f'{sim_path_prefix}.labels.csv'
+
+        model, model_type, root_state = draw_bdb_model(priors, rng)
+
+        seed = rng.random()
+
+        survived, root, burst_times = sdsdsim.model.sim_SDSD_tree(
+            rng_seed = seed,
+            sdsd_model = model,
+            root_state = root_state,
+            max_extant_leaves = max_extant_leaves,
+            max_extinct_leaves = max_extinct_leaves,
+            max_total_leaves = max_total_leaves,
+            max_time = max_time,
+        )
+
+        if (not survived) and (not keep_extinct_trees):
+            continue
+        if max_leaves_strict:
+            if (max_total_leaves and (root.number_of_leaves > max_total_leaves)):
+                sys.stderr.write(
+                    f"max_total_leaves is {max_total_leaves} and final shared event resulted "
+                    f"in {root.number_of_leaves} leaves...\n"
+                    f"\tDiscarding this simulation!\n"
+                )
+                continue
+            if (max_extant_leaves and (root.number_of_extant_leaves > max_extant_leaves)):
+                sys.stderr.write(
+                    f"max_extant_leaves is {max_extant_leaves} and final shared event resulted "
+                    f"in {root.number_of_extant_leaves} leaves...\n"
+                    f"\tDiscarding this simulation!\n"
+                )
+                continue
+            if (max_extinct_leaves and (root.number_of_extinct_leaves > max_extinct_leaves)):
+                # This should never happen, but putting logic in place in case
+                # we ever decide to allow shared extinction events
+                sys.stderr.write(
+                    f"max_extinct_leaves is {max_extinct_leaves} and final shared event resulted "
+                    f"in {root.number_of_extinct_leaves} leaves...\n"
+                    f"\tDiscarding this simulation!\n"
+                )
+                continue
+
+        assert root.number_of_extant_leaves == tree_width
+
+        if prune_extinct_leaves:
+            tree = tree.prune_extinct_leaves()
+
+        log10_variables = get_log10_model_parameters(model)
+        reduced_log10_variables = log10_variables
+        if label_names:
+            reduced_log10_variables = {k : log10_variables[k] for k in label_names}
+        reduced_log10_variables['model_type'] = model_type
+        reduced_log10_variables['root_state'] = root_state 
+        write_variables(labels_path, reduced_log10_variables)
+        write_tip_states(data_path, root, all_zeros = False)
+        with open(tree_path, 'w') as out_stream:
+            root.write_newick_simple(
+                out_stream,
+                include_root_annotations = include_root_annotations,
+            )
+            out_stream.write(";\n")
+        sim_idx += 1
+
+if __name__ == '__main__':
+    main()
